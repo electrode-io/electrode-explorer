8c71e32ed7d38ae62fae453b1b64d98c5d68cdfc
Dave Stevens <dstevens@walmartlabs.com>
2016-06-15 15:27:04 -0700

    WIP: POST-able URI for updating

---
+++ server/component-data/index.js
@@ -0,0 +1,72 @@
+"use strict";
+
+/* Handles updating (or creating) the component
+ * data for demoing. Implemented in the server
+ * as we want to be able to flash updates after
+ * the server is up and running, without requiring
+ * a restart */
+const GitHubApi = require("github");
+const Config = require("@walmart/electrode-ui-config");
+const ghToken = Config.ui.automaticUpdate && process.env.GHACCESS_TOKEN;
+const github = new GitHubApi({
+  version: "3.0.0",
+  protocol: "https",
+  host: "gecgithub01.walmart.com",
+  pathPrefix: "/api/v3"
+});
+
+const fetchRepo = (org, repoName) => {
+  github.authenticate({
+    type: "oauth",
+    token: ghToken
+  });
+
+  github.repos.getContent({
+    user: org,
+    repo: repoName,
+    path: "demo/index.jsx"
+  }, (err, response) => {
+    if (err) {
+      return err;
+    }
+
+    console.log(response);
+  });
+};
+
+const ComponentData = {};
+
+ComponentData.register = (server, options, next) => {
+
+  server.route({
+    path: "/portal/api/update/repo/{org}/{repoName}",
+    method: "POST",
+    config: {
+      plugins: {
+        "@walmart/csrf-jwt": {
+          enabled: false
+        }
+      }
+    },
+    handler: (request, reply) => {
+
+      if (!ghToken) {
+        // No token? No automatic updates.
+        return reply("Automatic updating requires a Github access token. No token found.");
+      }
+
+      fetchRepo(request.params.org, request.params.repoName);
+      return reply(`Update ${request.params.org}:${request.params.repoName} please`);
+    }
+  });
+
+  return next();
+
+};
+
+ComponentData.register.attributes = {
+  name: "portalComponentData",
+  version: "1.0.0"
+};
+
+module.exports = ComponentData;
