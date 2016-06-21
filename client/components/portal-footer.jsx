247a9957dc1956ac75f1529466d74f845142b8ce
Caoyang Shi <CShi@walmartlabs.com>
2016-06-21 00:54:16 -0700

    footer

--- client/components/portal-footer.jsx
+++ client/components/portal-footer.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 const PortalFooter = (props) => {
   return (
-    <div className="portal-footer" />
+    <div className="portal-footer">
+      <div className="footer-container">
+        <div className="footer-text">
+          © Electrode
+        </div>
+      </div>
+    </div>
   );
 };
 
