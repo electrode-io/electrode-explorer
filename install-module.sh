4fda100cb045d9bd534fd546771e16ff32fee4e9
Caoyang Shi <CShi@walmartlabs.com>
2016-06-22 18:03:58 -0700

    install module script

---
+++ install-module.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+  echo "No module name specified. Exiting."
+  exit 1
+fi
+
+if [ -f /usr/local/lib/electrode-ops-helpers.sh ]; then
+  source /usr/local/lib/electrode-ops-helpers.sh
+  prepare_nodejs 4.4.5
+  npm set registry https://npme.walmart.com/
+  npm set strict-ssl false
+fi
+
+npm i $1
