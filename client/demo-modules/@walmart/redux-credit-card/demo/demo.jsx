a636668e1a4a31378baab89f420a43bb5a70ff2b
Caoyang Shi <CShi@walmartlabs.com>
2016-07-06 20:59:45 -0700

    remove deprecated module

--- client/demo-modules/@walmart/redux-credit-card/demo/demo.jsx
+++ client/demo-modules/@walmart/redux-credit-card/demo/demo.jsx
@@ -5,7 +5,6 @@ import Playground from "component-playground";
 import creditCardStore from "./mock/credit-card-store";
 import mocks from "./mock/credit-cards-mock";
 import errorMock from "./mock/credit-cards-error-mock";
-import "@walmart/wmreact-react-demo-shared/shared.css";
 import "./demo.styl";
 
 const App = () => (
