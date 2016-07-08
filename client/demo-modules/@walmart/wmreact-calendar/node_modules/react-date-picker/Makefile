7232eec342b0e2c04f5e2ae657b51a6ef1ccfcfb
Caoyang Shi <CShi@walmartlabs.com>
2016-07-08 02:25:20 -0700

    update modules

--- client/demo-modules/@walmart/wmreact-calendar/node_modules/react-date-picker/Makefile
+++
@@ -1,9 +0,0 @@
-REPORTER = spec
-
-test:
-	npm run test
-test-w:
-	npm run test-w
-
-
-.PHONY: test test-w
--- client/demo-modules/@walmart/wmreact-calendar/node_modules/react-date-picker/lib/onEnter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function onKeyUp(fn) {
-  return function (event) {
-    if (event.key == 'Enter') {
-      fn(event);
-    }
-  };
-};
