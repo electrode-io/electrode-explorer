d5ce6c89c336b16a2e84fb015873a1c8196b8d54
Caoyang Shi <CShi@walmartlabs.com>
2016-07-13 14:07:31 -0700

    remove unnecessary files

--- post-install-module.sh
+++ post-install-module.sh
@@ -10,6 +10,6 @@ if [ ! -d client/demo-modules/$1 ]; then
 fi
 
 cp -r node_modules/$1 client/demo-modules/@walmart/
-rm client/demo-modules/$1/.babelrc
+rm client/demo-modules/$1/.babelrc client/demo-modules/$1/package.json client/demo-modules/$1/README.md client/demo-modules/$1/components* client/demo-modules/$1/.npmignore client/demo-modules/$1/.builderrc
 rm -rf client/demo-modules/$1/node_modules
 find client/demo-modules/$1 -name "*.flow" -type f -delete
