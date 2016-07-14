9c71c0268be742605d3ae184f69bb57bf970fd52
Caoyang Shi <CShi@walmartlabs.com>
2016-07-14 14:06:40 -0700

    script to import base styl

--- scripts/post-install-module.sh
+++ scripts/post-install-module.sh
@@ -9,7 +9,26 @@ if [ ! -d client/demo-modules/$1 ]; then
   mkdir -p client/demo-modules/$1
 fi
 
-cp -r node_modules/$1 client/demo-modules/@walmart/
-rm -rf client/demo-modules/$1/node_modules client/demo-modules/$1/lib client/demo-modules/$1/test client/demo-modules/$1/.idea client/demo-modules/$1/coverage
-rm client/demo-modules/$1/package.json client/demo-modules/$1/README.md client/demo-modules/$1/components* client/demo-modules/$1/.*
-find client/demo-modules/$1 -name "*.flow" -type f -delete
+function copy_and_clean() {
+  cp -r node_modules/$1 client/demo-modules/@walmart/
+  rm -rf client/demo-modules/$1/node_modules client/demo-modules/$1/lib client/demo-modules/$1/test client/demo-modules/$1/.idea client/demo-modules/$1/coverage
+  rm client/demo-modules/$1/package.json client/demo-modules/$1/README.md client/demo-modules/$1/components* client/demo-modules/$1/.*
+  find client/demo-modules/$1 -name "*.flow" -type f -delete
+}
+
+str='@require "~@walmart/lithe-extras/lib/tenants/walmart/base"'
+function import_stylus() {
+  files=$(find client/demo-modules/$1 -name "*.styl")
+  for file in $files
+  do
+    grep "$str" $file
+    if [ $? -ne 0 ]; then
+      mv $file $file.tmp
+      echo "$str;" | cat - $file.tmp > $file
+      rm $file.tmp
+    fi
+  done
+}
+
+copy_and_clean $1
+import_stylus $1
