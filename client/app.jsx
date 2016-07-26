91e6c61df7dd05eaaf90d7bca7007e9ebf8bee21
Caoyang Shi <CShi@walmartlabs.com>
2016-07-25 17:51:19 -0700

    remove canary files

--- client/app.jsx
+++ client/app.jsx
@@ -11,18 +11,10 @@ import { Resolver } from "react-resolver";
 import { createHistory } from "history";
 
 import { ElectrodeApplication } from "@walmart/electrode-application";
-import { setListener } from "@walmart/electrode-fetch";
-import { isBeaconMessage } from "@walmart/wmreact-analytics";
-
-import { addLogMessage } from "./log-stream";
-import { canary } from "./canary";
-import { canaryRules } from "./canary-rules";
 
 import "./styles/base.styl";
 
 const rootEl = document.querySelector(".js-content");
-
-const canaryMessage = (msg) => {};
 
 global.React = React;
 global.ReactDOM = ReactDOM;
@@ -30,10 +22,7 @@ global.ReactDOM = ReactDOM;
 // Note: Change suffix to `.js` if not using actual JSX.
 Resolver.render(
   () => (
-    <ElectrodeApplication
-      canary={canary}
-      canaryMessage={canaryMessage}
-      canaryRules={canaryRules}>
+    <ElectrodeApplication>
       <Router history={createHistory()}>{routes}</Router>
     </ElectrodeApplication>
   ),
--- client/canary-rules/index.js
+++
@@ -1,9 +0,0 @@
-import { findClicks } from "./find-clicks";
-import { findSplunkClick } from "./find-splunk-click";
-import { findLogmonClick } from "./find-logmon-click";
-
-export const canaryRules = [
-  findClicks,
-  findSplunkClick,
-  findLogmonClick
-];
