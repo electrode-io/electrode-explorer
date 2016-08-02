8c1307768e19206ad1d63e96274ff2914501da7b
Caoyang Shi <CShi@walmartlabs.com>
2016-08-02 14:22:52 -0700

    update scripts

--- scripts/info-module.sh
+++ scripts/info-module.sh
@@ -5,11 +5,12 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-if [ -f /usr/local/lib/electrode-ops-helpers.sh ]; then
+if which npm; then
+  npm info $1
+elif [ -f /usr/local/lib/electrode-ops-helpers.sh ]; then
   source /usr/local/lib/electrode-ops-helpers.sh
   prepare_nodejs 4
   npm set registry https://npme.walmart.com/
   npm set strict-ssl false
+  npm info $1
 fi
-
-npm info $1
