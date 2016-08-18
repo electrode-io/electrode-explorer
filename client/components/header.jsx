6176ead7df1312eea030f5c747e28a471308137a
cshi1 <cshi@walmartlabs.com>
2016-08-18 16:04:39 -0700

    explorer

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,9 +4,9 @@ import Config from "@walmart/electrode-ui-config";
 const explorerHeader = () => {
   return (
     <div className="explorer-header">
-      <h1 className="explorer-title">{Config.ui.meta.explorerLogo ?
-        <img src={Config.ui.meta.explorerLogo}/> :
-        Config.ui.meta.explorerName}
+      <h1 className="explorer-title">{Config.ui.meta.logo ?
+        <img src={Config.ui.meta.logo}/> :
+        Config.ui.meta.name}
       </h1>
     </div>
   );
