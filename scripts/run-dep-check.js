05943b47ac00e52ff170f1203a0de8d4730ef0fd
cshi1 <cshi@walmartlabs.com>
2016-08-18 14:51:12 -0700

    update scripts

--- scripts/run-dep-check.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const checkDependencies = require("../server/component-data/check-dependencies");
-
-const pkg = require("@walmart/wmreact-interactive/package.json");
-
-checkDependencies("react/interactive", pkg.dependencies, pkg.devDependencies);
