3198361a66059ae7446b74f7ea089f4f4eafc5d8
Caoyang Shi <CShi@walmartlabs.com>
2016-08-18 12:20:01 -0700

    update scripts

--- scripts/install-module.sh
+++ scripts/install-module.sh
@@ -13,8 +13,6 @@ function npm_install() {
   elif [ -f /usr/local/lib/electrode-ops-helpers.sh ]; then
     source /usr/local/lib/electrode-ops-helpers.sh
     prepare_nodejs 4
-    npm set registry https://npme.walmart.com/
-    npm set strict-ssl false
     npm set progress false
     npm i $1
     npm i --only=dev $1
