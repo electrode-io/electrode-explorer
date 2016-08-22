da6e579b6326b387194318470999607e920e2175
caoyangs <wuwa007@gmail.com>
2016-08-22 16:03:22 -0700

    remove unused variable

--- server/component-data/fetch-doc.js
+++ server/component-data/fetch-doc.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const GitHubApi = require("github");
-const Promise = require("bluebird");
 const Config = require("electrode-confippet").config;
 const github = new GitHubApi(Config.githubApi);
 const githubAuthObject = require("./utils/github-auth-object");
