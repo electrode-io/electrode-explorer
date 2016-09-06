4c6987a7e61ea957646814548c21d6e7d52789ec
caoyangs <wuwa007@gmail.com>
2016-09-06 15:14:58 -0700

    move poll out

--- server/poll.js
+++ server/poll.js
@@ -9,7 +9,7 @@ const GitHubApi = require("github");
 const Promise = require("bluebird");
 const Config = require("electrode-confippet").config;
 const github = new GitHubApi(Config.githubApi);
-const githubAuthObject = require("./utils/github-auth-object");
+const githubAuthObject = require("./component-data/utils/github-auth-object");
 
 const Poll = {};
 
