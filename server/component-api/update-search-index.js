6394c84dd7613380dc2dbeeaf79a2999858a48b0
caoyangs <wuwa007@gmail.com>
2016-09-08 15:10:10 -0700

    use promise

--- server/component-api/update-search-index.js
+++ server/component-api/update-search-index.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const Path = require("path");
-const Fs = require("fs");
+const fs = require("fs");
+const Promise = require("bluebird");
+const readFile = Promise.promisify(fs.readFile);
 const Chalk = require("chalk");
 
 const CreateSearchStrings = require("../search-api/create-search-strings");
@@ -12,28 +14,21 @@ const UpdateSearchIndex = (moduleName, subModules, server, keywords) => {
 
   let searchIndex = {};
 
-  try {
-    // If the index already exists, pull it in
-    searchIndex = require(IndexPath);
-  } catch (err) { }
+  readFile(IndexPath)
+    .then((data) => {
+      searchIndex = data;
 
-  searchIndex[moduleName] = (keywords || []).concat(subModules);
+      searchIndex[moduleName] = (keywords || []).concat(subModules);
 
-  // Directly update server app stored value so that it's available immediately
-  server.settings.app.searchIndex = searchIndex;
-  server.settings.app.searchStrings = CreateSearchStrings(searchIndex);
+      // Directly update server app stored value so that it's available immediately
+      server.settings.app.searchIndex = searchIndex;
+      server.settings.app.searchStrings = CreateSearchStrings(searchIndex);
 
-  // Write to file to persist data
-  Fs.writeFile(IndexPath, JSON.stringify(searchIndex), (err) => {
-    if (err) {
-      return console.log(Chalk.red("Error writing search index"), err);
-    }
-
-    console.log(Chalk.green(`Successfully wrote search index to ${IndexPath}`));
-
-  });
-
+      // Write to file to persist data
+      Fs.writeFileSync(IndexPath, JSON.stringify(searchIndex));
+      console.log(Chalk.green(`Successfully wrote search index to ${IndexPath}`));
+    })
+    .catch(() => {});
 };
 
 module.exports = UpdateSearchIndex;
-
